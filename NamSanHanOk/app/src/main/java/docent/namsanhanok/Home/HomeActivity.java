b4d76233c74aaf07b709132c5eb51c0b301c8684
Jooyeong <young951109@naver.com>
2018-07-16 18:06:44 +0900

    HomeActivity 토글버튼 추가

--- NamSanHanOk/app/src/main/java/docent/namsanhanok/Home/HomeActivity.java
+++ NamSanHanOk/app/src/main/java/docent/namsanhanok/Home/HomeActivity.java
@@ -3,6 +3,10 @@ package docent.namsanhanok.Home;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
+
+import com.github.angads25.toggle.LabeledSwitch;
+import com.github.angads25.toggle.interfaces.OnToggledListener;
 
 import docent.namsanhanok.Docent.DocentActivity;
 import docent.namsanhanok.R;
@@ -14,8 +18,19 @@ public class HomeActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
-        Intent intent = new Intent(this, DocentActivity.class);
-        startActivity(intent);
-        finish();
+        LabeledSwitch toggleBtn = findViewById(R.id.toggleBtn);
+        toggleBtn.setOnToggledListener(new OnToggledListener() {
+            @Override
+            public void onSwitched(LabeledSwitch labeledSwitch, boolean isOn) {
+                //toggle Button이 On일 때
+                Toast.makeText(HomeActivity.this, "ON", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+//        Intent intent = new Intent(this, DocentActivity.class);
+//        startActivity(intent);
+//        finish();
+
+
     }
 }
