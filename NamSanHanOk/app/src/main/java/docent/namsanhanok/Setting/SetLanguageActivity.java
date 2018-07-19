fe0d0dcfa404a74fb0f898ba051c516a60ec72dd
Jooyeong <young951109@naver.com>
2018-07-19 14:55:10 +0900

    SetLanguageActivity 언어 설정 아이템 선택 표시 가능

--- NamSanHanOk/app/src/main/java/docent/namsanhanok/Setting/SetLanguageActivity.java
+++ NamSanHanOk/app/src/main/java/docent/namsanhanok/Setting/SetLanguageActivity.java
@@ -1,13 +1,19 @@
 package docent.namsanhanok.Setting;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Checkable;
 import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import java.util.List;
 
 import docent.namsanhanok.Home.HomeActivity;
 import docent.namsanhanok.R;
@@ -31,25 +37,27 @@ public class SetLanguageActivity extends AppCompatActivity {
     }
 
     public void init() {
-        ListView languageListview = (ListView)findViewById(R.id.languageListview);
+        ListView languageListview = (ListView) findViewById(R.id.languageListview);
+
         ArrayAdapter<CharSequence> setLanguageAdapter
                 = ArrayAdapter.createFromResource(this, R.array.languages_array, android.R.layout.simple_list_item_single_choice);
         languageListview.setAdapter(setLanguageAdapter);
+        languageListview.setChoiceMode(ListView.CHOICE_MODE_SINGLE);
 
         languageListview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
                 switch (position) {
-                    case 0 : //한국어
+                    case 0: //한국어
                         Toast.makeText(SetLanguageActivity.this, "한국어", Toast.LENGTH_SHORT).show();
                         break;
-                    case 1 : //영어
+                    case 1: //영어
                         Toast.makeText(SetLanguageActivity.this, "영어", Toast.LENGTH_SHORT).show();
                         break;
-                    case 2 : //일본어
+                    case 2: //일본어
                         Toast.makeText(SetLanguageActivity.this, "일본어", Toast.LENGTH_SHORT).show();
                         break;
-                    case 3 : //중국어
+                    case 3: //중국어
                         Toast.makeText(SetLanguageActivity.this, "중국어", Toast.LENGTH_SHORT).show();
                         break;
                 }
