8e4a08ac1b226451a327beb1fdeeb82408b960a2
Jooyeong <young951109@naver.com>
2018-07-19 16:04:50 +0900

    HomeActivity 알리는 말씀 메뉴 버튼 및 NoticeActivity 레이아웃 추가

--- NamSanHanOk/app/src/main/java/docent/namsanhanok/Home/HomeActivity.java
+++ NamSanHanOk/app/src/main/java/docent/namsanhanok/Home/HomeActivity.java
@@ -7,22 +7,22 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.ImageView;
-import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 import android.widget.Toast;
 import com.github.angads25.toggle.LabeledSwitch;
 import com.github.angads25.toggle.interfaces.OnToggledListener;
 import docent.namsanhanok.Category.CategoryActivity;
-import docent.namsanhanok.Category.CategoryListActivity;
 
 import docent.namsanhanok.Event.EventActivity;
+import docent.namsanhanok.Notice.NoticeActivity;
+import docent.namsanhanok.Question.QuestionWriteActivity;
 import docent.namsanhanok.R;
 import docent.namsanhanok.Setting.SettingActivity;
 
 public class HomeActivity extends AppCompatActivity {
 
     ImageButton settingBtn;
-    ImageView menuBtn1, menuBtn2, menuBtn3, menuBtn4;
+    ImageView menuBtn1, menuBtn2, menuBtn3, menuBtn4, menuBtn5;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,9 +40,6 @@ public class HomeActivity extends AppCompatActivity {
             }
         });
 
-//        Intent intent = new Intent(this, DocentActivity.class);
-//        startActivity(intent);
-//        finish();
     }
 
     public void onClick(View v) {
@@ -64,11 +61,17 @@ public class HomeActivity extends AppCompatActivity {
                 startActivity(intent);
                 break;
 
-            case R.id.menuBtn3 : //질문게시판
-                Toast.makeText(this, "질문게시판", Toast.LENGTH_SHORT).show();
+            case R.id.menuBtn3 : //알리는 말씀
+                intent = new Intent(HomeActivity.this, NoticeActivity.class);
+                startActivity(intent);
                 break;
 
-            case R.id.menuBtn4 : //이용안내
+            case R.id.menuBtn4 : //문의하기
+                intent = new Intent(HomeActivity.this, QuestionWriteActivity.class);
+                startActivity(intent);
+                break;
+
+            case R.id.menuBtn5 : //안내
                 Toast.makeText(this, "이용안내", Toast.LENGTH_SHORT).show();
                 break;
         }
@@ -84,6 +87,7 @@ public class HomeActivity extends AppCompatActivity {
         menuBtn2 = (ImageView)findViewById(R.id.menuBtn2);
         menuBtn3 = (ImageView)findViewById(R.id.menuBtn3);
         menuBtn4 = (ImageView)findViewById(R.id.menuBtn4);
+        menuBtn5 = (ImageView)findViewById(R.id.menuBtn5);
 
         RelativeLayout homeLayout = (RelativeLayout)findViewById(R.id.homeLayout);
         homeLayout.getBackground().setAlpha(220);
--- NamSanHanOk/app/src/main/java/docent/namsanhanok/Notice/NoticeActivity.java
+++ NamSanHanOk/app/src/main/java/docent/namsanhanok/Notice/NoticeActivity.java
@@ -1,16 +1,15 @@
-package docent.namsanhanok.Question;
+package docent.namsanhanok.Notice;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import docent.namsanhanok.R;
 
-public class QuestionBoardActivity extends AppCompatActivity {
+public class NoticeActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_question_board);
-
+        setContentView(R.layout.activity_notice);
     }
 }
