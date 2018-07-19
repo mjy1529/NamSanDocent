fc8d39a417468529a9d4c17ca3340b0c3fc4af9e
DESKTOP-FFLFGB0\ceoah <ceoahn95@gmail.com>
2018-07-19 15:01:33 +0900

    질문게시판 폴더 생성

---
+++ NamSanHanOk/app/src/main/java/docent/namsanhanok/Question/QuestionWriteActivity.java
@@ -0,0 +1,16 @@
+package docent.namsanhanok.Question;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import docent.namsanhanok.R;
+
+public class QuestionWriteActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_questionwrite);
+
+    }
+}
