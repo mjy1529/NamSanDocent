37164c9fd5af74a315715799774e52a3538607ca
DESKTOP-FFLFGB0\ceoah <ceoahn95@gmail.com>
2018-07-19 15:11:33 +0900

    question write toolbar 생성

--- NamSanHanOk/app/src/main/java/docent/namsanhanok/Question/QuestionWriteActivity.java
+++ NamSanHanOk/app/src/main/java/docent/namsanhanok/Question/QuestionWriteActivity.java
@@ -2,6 +2,7 @@ package docent.namsanhanok.Question;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 import docent.namsanhanok.R;
 
@@ -11,6 +12,14 @@ public class QuestionWriteActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_questionwrite);
+
+        init();
+    }
+
+    public void init() {
+        Toolbar questionRegisterToolbar = (Toolbar)findViewById(R.id.question_register_toolbar);
+        questionRegisterToolbar.bringToFront();
+
 
     }
 }
