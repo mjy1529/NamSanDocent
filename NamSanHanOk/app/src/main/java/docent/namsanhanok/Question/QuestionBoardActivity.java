58c371c53b4e656538f704210aa4fe0b838ceef6
Jooyeong <young951109@naver.com>
2018-07-19 15:11:12 +0900

    QuestionBoardAcitivy 추가

---
+++ NamSanHanOk/app/src/main/java/docent/namsanhanok/Question/QuestionBoardActivity.java
@@ -0,0 +1,16 @@
+package docent.namsanhanok.Question;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import docent.namsanhanok.R;
+
+public class QuestionBoardActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_question_board);
+
+    }
+}
