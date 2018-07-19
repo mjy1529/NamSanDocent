e16d4e936af20abcf11a82501607493c125d7d26
Jooyeong <young951109@naver.com>
2018-07-19 10:00:57 +0900

    SetLanguageActivity 언어 설정 아이템 클릭리스너 추가

--- NamSanHanOk/app/src/main/java/docent/namsanhanok/Setting/SetLanguageActivity.java
+++ NamSanHanOk/app/src/main/java/docent/namsanhanok/Setting/SetLanguageActivity.java
@@ -7,6 +7,7 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import docent.namsanhanok.Home.HomeActivity;
 import docent.namsanhanok.R;
@@ -31,7 +32,28 @@ public class SetLanguageActivity extends AppCompatActivity {
 
     public void init() {
         ListView languageListview = (ListView)findViewById(R.id.languageListview);
-        ArrayAdapter<CharSequence> setLanguageAdapter = ArrayAdapter.createFromResource(this, R.array.languages_array, android.R.layout.simple_list_item_single_choice);
+        ArrayAdapter<CharSequence> setLanguageAdapter
+                = ArrayAdapter.createFromResource(this, R.array.languages_array, android.R.layout.simple_list_item_single_choice);
         languageListview.setAdapter(setLanguageAdapter);
+
+        languageListview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
+                switch (position) {
+                    case 0 : //한국어
+                        Toast.makeText(SetLanguageActivity.this, "한국어", Toast.LENGTH_SHORT).show();
+                        break;
+                    case 1 : //영어
+                        Toast.makeText(SetLanguageActivity.this, "영어", Toast.LENGTH_SHORT).show();
+                        break;
+                    case 2 : //일본어
+                        Toast.makeText(SetLanguageActivity.this, "일본어", Toast.LENGTH_SHORT).show();
+                        break;
+                    case 3 : //중국어
+                        Toast.makeText(SetLanguageActivity.this, "중국어", Toast.LENGTH_SHORT).show();
+                        break;
+                }
+            }
+        });
     }
 }
