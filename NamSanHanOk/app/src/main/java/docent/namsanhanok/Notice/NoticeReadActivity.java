8d4a88f77754826ac28ff5e1ca53994bd8b663d9
Jooyeong <young951109@naver.com>
2018-07-20 18:05:05 +0900

    NoticeReadActivity 운영자용 more 버튼에 PopupMenu 추가

--- NamSanHanOk/app/src/main/java/docent/namsanhanok/Notice/NoticeReadActivity.java
+++ NamSanHanOk/app/src/main/java/docent/namsanhanok/Notice/NoticeReadActivity.java
@@ -4,13 +4,21 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.ImageButton;
+import android.widget.PopupMenu;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import docent.namsanhanok.Home.HomeActivity;
 import docent.namsanhanok.R;
 
 public class NoticeReadActivity extends AppCompatActivity {
+
+    ImageButton moreBtn;
 
     TextView notice_title_tv;
     TextView notice_date_tv;
@@ -28,27 +36,56 @@ public class NoticeReadActivity extends AppCompatActivity {
     public void onClick(View v) {
         Intent intent;
         switch (v.getId()) {
-            case R.id.listBtn :
+            case R.id.listBtn:
                 intent = new Intent(this, NoticeActivity.class);
                 startActivity(intent);
                 finish();
                 break;
-            case R.id.moreBtn :
+            case R.id.moreBtn:
+                Log.d("more", "click");
+                PopupMenu popupMenu = new PopupMenu(getApplicationContext(), v);
+                getMenuInflater().inflate(R.menu.notice_admin_menu, popupMenu.getMenu());
+                popupMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+                    @Override
+                    public boolean onMenuItemClick(MenuItem menuItem) {
+                        switch (menuItem.getItemId()) {
+                            case R.id.modify:
+                                Toast.makeText(NoticeReadActivity.this, "modify", Toast.LENGTH_SHORT).show();
+                                break;
+                            case R.id.remove:
+                                Toast.makeText(NoticeReadActivity.this, "remove", Toast.LENGTH_SHORT).show();
+                                break;
+                        }
+                        return false;
+                    }
+                });
+                popupMenu.show();
                 break;
         }
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        return super.onOptionsItemSelected(item);
+    }
+
     public void init() {
-        Toolbar noticeReadToolbar = (Toolbar)findViewById(R.id.noticeReadToolbar);
+        Toolbar noticeReadToolbar = (Toolbar) findViewById(R.id.noticeReadToolbar);
         setSupportActionBar(noticeReadToolbar);
 
+        moreBtn = (ImageButton)findViewById(R.id.moreBtn);
         notice_title_tv = (TextView) findViewById(R.id.notice_title_tv);
         notice_date_tv = (TextView) findViewById(R.id.notice_date_tv);
         notice_readCnt_tv = (TextView) findViewById(R.id.notice_readCnt_tv);
         notice_content_tv = (TextView) findViewById(R.id.notice_content_tv);
 
         Intent intent = getIntent();
-        NoticeRecyclerItem object = (NoticeRecyclerItem)intent.getSerializableExtra("object");
+        NoticeRecyclerItem object = (NoticeRecyclerItem) intent.getSerializableExtra("object");
 
         notice_title_tv.setText(object.getTitle());
         notice_date_tv.setText(object.getDate());
