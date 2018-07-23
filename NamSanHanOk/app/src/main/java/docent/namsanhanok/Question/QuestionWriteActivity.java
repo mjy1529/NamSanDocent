d4e951669e57763769738e211cd5e75e83ac21c8
DESKTOP-FFLFGB0\ceoah <ceoahn95@gmail.com>
2018-07-23 15:01:35 +0900

    문의하기 box color change완료

--- NamSanHanOk/app/src/main/java/docent/namsanhanok/Question/QuestionWriteActivity.java
+++ NamSanHanOk/app/src/main/java/docent/namsanhanok/Question/QuestionWriteActivity.java
@@ -1,23 +1,32 @@
 package docent.namsanhanok.Question;
 
+import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.text.TextUtils;
+import android.util.AttributeSet;
+import android.view.KeyEvent;
 import android.view.View;
+import android.view.inputmethod.EditorInfo;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.LinearLayout;
 import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import java.security.Key;
 import java.util.ArrayList;
 
 import docent.namsanhanok.R;
 import me.srodrigo.androidhintspinner.HintAdapter;
 import me.srodrigo.androidhintspinner.HintSpinner;
 
-public class QuestionWriteActivity extends AppCompatActivity {
+public class QuestionWriteActivity extends AppCompatActivity implements EditText.OnFocusChangeListener {
     Button post_register_Btn;
     ImageButton cancelBtn;
 
@@ -33,10 +42,11 @@ public class QuestionWriteActivity extends AppCompatActivity {
     EditText username;
     EditText title;
     EditText content;
+    LinearLayout content_layout;
 
     ArrayList<String> nullValue;
 
-
+    InputMethodManager imm;
 
 
     @Override
@@ -48,7 +58,29 @@ public class QuestionWriteActivity extends AppCompatActivity {
 
         init();
         setCategorySpinner();
+        email_first_address.setBackgroundResource(R.drawable.rectangle_edge_all_selected);
+
+
     }
+
+
+//    public void getFocus(){
+//        if(getCurrentFocus() == email_first_address){
+//            // your view is in focus
+//            email_first_address.setBackgroundResource(R.drawable.rectangle_edge_all_selected);
+//        }else{
+//            // not in the focus
+//            email_first_address.setBackgroundResource(R.drawable.rectangle_edge_all);
+//        }
+//
+//        if(getCurrentFocus() == phone_number){
+//            // your view is in focus
+//            phone_number.setBackgroundResource(R.drawable.rectangle_edge_all_selected);
+//        }else{
+//            // not in the focus
+//            phone_number.setBackgroundResource(R.drawable.rectangle_edge_all);
+//        }
+//    }
 
     //spinner hint 설정
     public void setSpinner(final Spinner spinner, final String hint, ArrayList<String> spinner_list) {
@@ -70,6 +102,8 @@ public class QuestionWriteActivity extends AppCompatActivity {
 
 
     }
+
+
 
     public void setCategorySpinner() {
 
@@ -119,11 +153,68 @@ public class QuestionWriteActivity extends AppCompatActivity {
 
         question_category_spinner = (Spinner) findViewById(R.id.question_category_Spinner);
 //        question_category_spinner.performClick();
+        question_category_spinner.setFocusable(true);
+        question_category_spinner.setFocusableInTouchMode(true);
 
         title = (EditText) findViewById(R.id.question_title);
         content = (EditText) findViewById(R.id.question_content);
+        content_layout = (LinearLayout) findViewById(R.id.question_content_layout);
+
+
+
+        //Focus가 변할 때, line color 변경
+        email_first_address.setOnFocusChangeListener(this);
+        phone_number.setOnFocusChangeListener(this);
+        username.setOnFocusChangeListener(this);
+        question_category_spinner.setOnFocusChangeListener(this);
+        title.setOnFocusChangeListener(this);
+        content.setOnFocusChangeListener(this);
+
+
+        imm = (InputMethodManager)getSystemService(INPUT_METHOD_SERVICE);
+
+
 
     }
+
+    @Override
+    public void onFocusChange(View v, boolean hasFocus) {
+        switch(v.getId()){
+            case R.id.question_email_first_address :
+                email_first_address.setBackgroundResource(R.drawable.rectangle_edge_all_selected);
+                if(getCurrentFocus()!=email_first_address){email_first_address.setBackgroundResource(R.drawable.rectangle_edge_all);}
+                break;
+
+            case R.id.question_phone_number :
+                phone_number.setBackgroundResource(R.drawable.rectangle_edge_all_selected);
+                if(getCurrentFocus()!=phone_number){phone_number.setBackgroundResource(R.drawable.rectangle_edge_all);}
+
+                break;
+
+            case R.id.question_username :
+                username.setBackgroundResource(R.drawable.rectangle_edge_all_selected);
+                if(getCurrentFocus()!=username){username.setBackgroundResource(R.drawable.rectangle_edge_all);}
+                break;
+
+            case R.id.question_category_Spinner :
+                question_category_spinner.setBackgroundResource(R.drawable.rectangle_edge_all_selected);
+                imm.hideSoftInputFromWindow(question_category_spinner.getWindowToken(), 0);
+                if(getCurrentFocus()!=question_category_spinner){question_category_spinner.setBackgroundResource(R.drawable.rectangle_edge_all);}
+                break;
+
+            case R.id.question_title :
+                title.setBackgroundResource(R.drawable.rectangle_edge_all_selected);
+                if(getCurrentFocus()!=title){title.setBackgroundResource(R.drawable.rectangle_edge_all);}
+                break;
+
+            case R.id.question_content :
+                content_layout.setBackgroundResource(R.drawable.rectangle_edge_all_selected);
+                if(getCurrentFocus()!=content){content_layout.setBackgroundResource(R.drawable.rectangle_edge_all);}
+                break;
+
+        }
+    }
+
 
 //비어잇으면 상자 표시 붉게
     public boolean detectError() {
@@ -143,6 +234,11 @@ public class QuestionWriteActivity extends AppCompatActivity {
             title.requestFocus();
             i++;
         }
+        if(selected_spinner==null){
+            nullValue.add("구분을 선택해주세요");
+            question_category_spinner.requestFocus();
+//            question_category_spinner.performClick();
+        }
         if(username.getText().toString().getBytes().length <= 0){
             nullValue.add("성함을 입력해주세요");
             username.requestFocus();
@@ -155,18 +251,10 @@ public class QuestionWriteActivity extends AppCompatActivity {
         }
         if(email_first_address.getText().toString().getBytes().length <= 0){
 
-//            nullValue.add("이메일을 입력해주세요");
+            nullValue.add("이메일을 입력해주세요");
             email_first_address.requestFocus();
             i++;
         }
-        if(nullValue.size() <=0 ){
-            if(selected_spinner==null){
-                nullValue.add("구분을 선택해주세요");
-                question_category_spinner.requestFocus();
-                question_category_spinner.performClick();
-            }
-        }
-
 
 
         if(nullValue.size() <= 0 ){ //빈칸이 없음
@@ -204,20 +292,19 @@ public class QuestionWriteActivity extends AppCompatActivity {
                     break;
                 }
                 else{ //빈칸이 있으면
+
+//                    String nullStrings = "";
+//                    for (int i = 0; i < nullValue.size() ; i++) {
+//                        if(i == nullValue.size()-1){
+//                            nullStrings += nullValue.get(i).toString();
+//                        }
+//                        else{
+//                            nullStrings += nullValue.get(i).toString() + "\n";
+//                        }
 //
-////                    String nullStrings = "";
-////                    for (int i = 0; i < nullValue.size() ; i++) {
-////                        if(i == nullValue.size()-1){
-////                            nullStrings += nullValue.get(i).toString();
-////                        }
-////                        else{
-////                            nullStrings += nullValue.get(i).toString() + "\n";
-////                        }
-////
-////                    }
-////                    Toast.makeText(this, nullStrings, Toast.LENGTH_LONG).show();
-//
-//
+//                    }
+                    Toast.makeText(this, nullValue.get(nullValue.size()-1).toString(), Toast.LENGTH_SHORT).show();
+
                     break;
                 }
 
