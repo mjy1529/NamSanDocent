526bdbc07da425ee59fc601024e7f87ef5033bb2
jooyeong <young951109@naver.com>
2018-08-02 17:37:06 +0900

    SplashActivity 구현

--- NamSanHanOk/app/src/main/java/docent/namsanhanok/Home/SplashActivity.java
+++ NamSanHanOk/app/src/main/java/docent/namsanhanok/Home/SplashActivity.java
@@ -1,8 +1,11 @@
 package docent.namsanhanok.Home;
 
+import android.content.Intent;
+import android.graphics.Typeface;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import docent.namsanhanok.R;
 
@@ -13,12 +16,21 @@ public class SplashActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
 
+        TextView splash_title = (TextView) findViewById(R.id.splash_title);
+        TextView splash_title2 = (TextView) findViewById(R.id.splash_title2);
+        Typeface typeface = Typeface.createFromAsset(getAssets(), "NanumMyeongjo.otf");
+        splash_title.setTypeface(typeface);
+        splash_title2.setTypeface(typeface);
+
         Handler handler = new Handler();
         handler.postDelayed(new Runnable() {
             @Override
             public void run() {
+                Intent intent = new Intent(SplashActivity.this, HomeActivity.class);
+                startActivity(intent);
                 finish();
             }
-        }, 2000);
+        }, 3000);
     }
+
 }
