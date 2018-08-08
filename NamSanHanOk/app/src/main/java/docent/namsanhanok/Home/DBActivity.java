a763de312b60f68112d38e117355fbe1ea46a085
Nayeon <ceoahn95@gmail.com>
2018-08-08 13:15:23 +0900

    Tutorial 수정, DBActivity 생성

---
+++ NamSanHanOk/app/src/main/java/docent/namsanhanok/Home/DBActivity.java
@@ -0,0 +1,15 @@
+package docent.namsanhanok.Home;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import docent.namsanhanok.R;
+
+public class DBActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_db);
+    }
+}
--- NamSanHanOk/app/src/main/java/docent/namsanhanok/Home/TutorialActivity.java
+++ NamSanHanOk/app/src/main/java/docent/namsanhanok/Home/TutorialActivity.java
@@ -19,63 +19,18 @@ import uk.co.deanwild.materialshowcaseview.target.Target;
 
 public class TutorialActivity extends AppCompatActivity {
 
-    private static final String SHOWCASE_ID = "tutorial";
     LinearLayout tuto_layout;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
+        setContentView(R.layout.activity_tutorial);
 
         init();
 
-        presentShowcaseSequence();
     }
 
-
-
-    private void presentShowcaseSequence() {
-
-        ShowcaseConfig config = new ShowcaseConfig();
-        config.setDelay(500); // half second between each showcase view
-
-        MaterialShowcaseSequence sequence = new MaterialShowcaseSequence(this, SHOWCASE_ID);
-
-
-        sequence.setOnItemDismissedListener(new MaterialShowcaseSequence.OnSequenceItemDismissedListener() {
-            @Override
-            public void onDismiss(MaterialShowcaseView materialShowcaseView, int i) {
-                if(i == 0){
-                    Intent intent = new Intent(getApplicationContext(), HomeActivity.class);
-                    startActivity(intent);
-                    finish();
-                }
-            }
-        });
-
-
-        sequence.setConfig(config);
-
-        sequence.addSequenceItem(tuto_layout, "자동전시안내를 원하시면 " + "\n" + "블루투스를 켜주세요", "완료");
-//        sequence.addSequenceItem((MaterialShowcaseView) Target.NONE, "튜토리얼을 시작하겠습니다.", "다음"); //안됨
-
-
-//        sequence.addSequenceItem(
-//                new MaterialShowcaseView.Builder(this)
-//                .setTarget(autoPlaytext)
-//                .setDismissText("다음")
-//                .setContentText("자동전시를 원하시면 블루투스를 켜주세요")
-//                .singleUse(SHOWCASE_ID) // provide a unique ID used to ensure it is only shown once
-//                .setMaskColour(getResources().getColor(R.color.light_green))
-//                .setShapePadding(20)
-//                .build()
-//        );
-
-
-        sequence.start();
-
-    }
 
 
 
