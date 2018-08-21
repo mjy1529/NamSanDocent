b3260f5477c145f8a1f2ac8d713247c1bf3f0a10
jooyeong <young951109@naver.com>
2018-08-21 13:54:06 +0900

    CategoryListActivity 비콘 알림 다이얼로그 추가 (비콘인식은 아직 안됨) 및 DocentActivity 레이아웃 수정

--- NamSanHanOk/app/src/main/java/docent/namsanhanok/Category/CategoryListActivity.java
+++ NamSanHanOk/app/src/main/java/docent/namsanhanok/Category/CategoryListActivity.java
@@ -1,72 +1,60 @@
 package docent.namsanhanok.Category;
 
-import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Environment;
 import android.os.Handler;
-import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
-import android.util.Log;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-
 import com.bumptech.glide.Glide;
-import com.codesgood.views.JustifiedTextView;
 import com.tsengvn.typekit.TypekitContextWrapper;
-
-import org.json.JSONException;
-import org.json.JSONObject;
-import org.w3c.dom.Text;
 
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Iterator;
-import java.util.Locale;
 
 import docent.namsanhanok.Application;
+import docent.namsanhanok.Docent.DocentActivity;
 import docent.namsanhanok.Docent.DocentData;
-import docent.namsanhanok.Docent.DocentResult;
 import docent.namsanhanok.Home.HomeActivity;
 import docent.namsanhanok.Manager.DocentMemList;
+import docent.namsanhanok.Manager.IDInfoData;
 import docent.namsanhanok.NetworkService;
 import docent.namsanhanok.R;
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
+import libs.mjn.prettydialog.PrettyDialog;
+import libs.mjn.prettydialog.PrettyDialogCallback;
 
-public class CategoryListActivity extends Activity {
+public class CategoryListActivity extends AppCompatActivity {
     private RecyclerView recyclerView;
     private LinearLayoutManager linearLayoutManager;
     private CategoryListAdapter categoryListAdapter;
 
     ImageButton homeBtn;
     TextView category_list_toolbar_title;
-    //    String category_title;
     ImageView simple_image;
     TextView category_list_title;
     TextView countText;
     TextView category_text_info;
 
-
     private NetworkService service;
     private ArrayList<DocentData> docentDataList;
-    private ArrayList<CategoryData> categoryDataList;
 
-    private Application applicationclass;
     DocentMemList docentMemList;
     CategoryData categoryData;
     DocentData docentData;
 
+    PrettyDialog newItemDialog = null;
+
     @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_category_list);
 
@@ -76,24 +64,30 @@ public class CategoryListActivity extends Activity {
 
         init();
 
-        setCategoryContent();
-        setDocentList();
+        setCategoryContent(categoryData);
+        setDocentList(categoryData);
 
+        // *** 테스트용 코드 : 비콘알림코드 추가 후 삭제 *** //
+        IDInfoData idInfoData = new IDInfoData();
+        idInfoData.category_id = "2";
+        idInfoData.docent_id = "4";
+        // ******************************** //
+
+        showNewItemDialog(idInfoData);
     }
 
-    public void setCategoryContent() {
+    public void setCategoryContent(CategoryData categoryData) {
         //카테고리 대표 이미지 설정
         Glide.with(CategoryListActivity.this)
                 .load(Environment.getExternalStorageDirectory() + categoryData.category_image_url)
                 .into(simple_image);
 
-        //카테고리 설명
-        String content = categoryData.category_detail_info;
-        category_text_info.setText(content);
-
+        category_list_toolbar_title.setText(categoryData.category_title); //툴바 타이틀
+        category_list_title.setText(categoryData.category_title + " 소개"); //소개 타이틀
+        category_text_info.setText(categoryData.category_detail_info); //카테고리 설명
     }
 
-    public void setDocentList() {
+    public void setDocentList(CategoryData categoryData) {
         HashMap<String, DocentData> map = new HashMap<String, DocentData>();
         docentMemList.get_docent_info(categoryData.category_id, map);
         docentDataList = new ArrayList<>();
@@ -101,8 +95,6 @@ public class CategoryListActivity extends Activity {
         Iterator<String> keys = map.keySet().iterator();
         while (keys.hasNext()) {
             String key = keys.next();
-            Log.d("check1", "key값 " + key);
-            Log.d("check1", "map.get(key) " + map.get(key));
             docentData = map.get(key);
             docentDataList.add(docentData);
         }
@@ -112,17 +104,65 @@ public class CategoryListActivity extends Activity {
         countText.setText(count);
     }
 
+    public void showNewItemDialog(final IDInfoData idInfoData) {
+        newItemDialog = new PrettyDialog(CategoryListActivity.this);
+        newItemDialog.setMessage(getResources().getString(R.string.newItemAlertMessage))
+                .setIcon(R.drawable.pdlg_icon_info)
+                .setIconTint(R.color.pdlg_color_blue)
+                .addButton("확인", // button text
+                        R.color.pdlg_color_white,  // button text color
+                        R.color.pdlg_color_blue,  // button background color
+                        new PrettyDialogCallback() {  // button OnClick listener
+                            @Override
+                            public void onClick() {
+                                if (!idInfoData.category_id.equals("")) {
+                                    if (idInfoData.docent_id.equals("")) {
+                                        CategoryData categoryData = new CategoryData();
+                                        docentMemList.get_category_info(idInfoData.category_id, categoryData);
+
+                                        setCategoryContent(categoryData);
+                                        setDocentList(categoryData);
+
+                                    } else if (!idInfoData.docent_id.equals("")) {
+                                        Intent intent = new Intent(CategoryListActivity.this, DocentActivity.class);
+
+                                        HashMap<String, DocentData> map = new HashMap<>();
+                                        docentMemList.get_docent_info(idInfoData.category_id, map);
+                                        DocentData docentData = map.get(idInfoData.docent_id);
+
+                                        intent.putExtra("docentObject", docentData);
+                                        startActivity(intent);
+                                    }
+                                    newItemDialog.dismiss();
+                                }
+                            }
+                        }
+                )
+                .addButton("취소", // button text
+                        R.color.pdlg_color_white,  // button text color
+                        R.color.dialog_cancel,  // button background color
+                        new PrettyDialogCallback() {  // button OnClick listener
+                            @Override
+                            public void onClick() {
+                                newItemDialog.dismiss();
+                            }
+                        }
+                );
+        newItemDialog.show();
+    }
 
     public void init() {
-        applicationclass = (Application) getApplicationContext();
         docentMemList = DocentMemList.getInstance();
 
-        category_list_toolbar_title = (TextView) findViewById(R.id.docentTitle);
         Toolbar categoryListToolbar = (Toolbar) findViewById(R.id.category_list_Toolbar);
-        categoryListToolbar.bringToFront();
+        setSupportActionBar(categoryListToolbar);
 
         homeBtn = (ImageButton) findViewById(R.id.homeBtn);
-        category_list_toolbar_title.setText(categoryData.category_title);
+        simple_image = (ImageView) findViewById(R.id.simple_image);
+        category_list_title = (TextView) findViewById(R.id.category_list_title);
+        countText = (TextView) findViewById(R.id.countText);
+        category_text_info = (TextView) findViewById(R.id.category_text_info);
+        category_list_toolbar_title = (TextView) findViewById(R.id.docentTitle);
 
         recyclerView = (RecyclerView) findViewById(R.id.category_list_recyclerView);
         linearLayoutManager = new LinearLayoutManager(this);
@@ -131,19 +171,8 @@ public class CategoryListActivity extends Activity {
         recyclerView.setLayoutManager(linearLayoutManager);
         recyclerView.setHasFixedSize(true);
 
-
         categoryListAdapter = new CategoryListAdapter(getApplicationContext(), docentDataList);
         recyclerView.setAdapter(categoryListAdapter);
-
-        simple_image = (ImageView) findViewById(R.id.simple_image);
-
-        category_list_title = (TextView) findViewById(R.id.category_list_title);
-        category_list_title.setText(categoryData.category_title + " 소개");
-        countText = (TextView) findViewById(R.id.countText);
-
-        category_text_info = (TextView) findViewById(R.id.category_text_info);
-
-
     }
 
 
--- NamSanHanOk/app/src/main/java/docent/namsanhanok/Home/HomeActivity.java
+++ NamSanHanOk/app/src/main/java/docent/namsanhanok/Home/HomeActivity.java
@@ -244,17 +244,21 @@ public class HomeActivity extends AppCompatActivity {
 //                        List<MinewBeacon> minewBeacons1 = null;
 //                        minewBeacons1.addAll(minewBeacons);
 //                    }
-//                    synchronized (minewBeacons)
+//                    synchronized (minewBeacons) {
 //                    IDInfoData idInfoData = new IDInfoData();
 //                    String beacon_minor = minewBeacons.get(i).getBeaconValue(BeaconValueIndex.MinewBeaconValueIndex_Minor).getStringValue();
 //                    int beacon_rssi = minewBeacons.get(i).getBeaconValue(BeaconValueIndex.MinewBeaconValueIndex_RSSI).getIntValue();
 //                    if (docentMemList.check_beacon_number(beacon_minor, idInfoData))
 //                }
+
 //                if (processing == false) {
 //                    processing = true;
 //                    criticalsection start;
-//                    List<MinewBeacon> minewBeaconsTemp;
-//                    minewBeaconsTemp.addTotal(minewBeacons);
+//                    synchronized (this) {
+//
+//                    }
+//                    ArrayList<MinewBeacon> minewBeaconsTemp = new ArrayList<>();
+//                    minewBeaconsTemp.addAll(minewBeacons);
 //                    addAppearBeacon(minewBeacons);
 //                    Log.d("beacon", minewBeacons.toString());
 //                    criticalsection end;
